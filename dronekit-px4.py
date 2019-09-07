bd9c6073cb1d108f3d6f1f40c795dd5de43fa39e
liu2z2 <55007630+liu2z2@users.noreply.github.com>
2019-09-06 23:28:34 -0400

    Add files via upload

---
+++ dronekit-px4.py
@@ -0,0 +1,21 @@
+# Import DroneKit-Python
+from dronekit import connect, Command, LocationGlobal
+from pymavlink import mavutil
+import time, sys, argparse, math
+
+# Connect to the Vehicle
+print "Connecting"
+connection_string = '127.0.0.1:14540'
+vehicle = connect(connection_string, wait_ready=True)
+
+i = 0
+
+while (i<10)
+	# Display basic vehicle state
+	print " Type: %s" % vehicle._vehicle_type
+	print " Armed: %s" % vehicle.armed
+	print " System status: %s" % vehicle.system_status.state
+	print " GPS: %s" % vehicle.gps_0
+	print " Alt: %s" % vehicle.location.global_relative_frame.alt
+	time.sleep(1000)
+	i+=1
